134f248a350fd756a01678ff18a58a506e1bc26e
Vibhugoell <146985689+Vibhugoell@users.noreply.github.com>
2025-02-24 17:09:09 +0530

    Update s.js

--- s.js
+++ s.js
@@ -1,11 +1,25 @@
 const express=require("express");
 const app=express();
 
+app.use(express.urlencoded({extended:true}))
+let userdata=[]
+
 //route to get signup page 
 app.get("/adduser",(req,res)=>{
     res.sendFile(__dirname+"/reg.html");
 })
 
+app.post("/adduser", (req,res)=>{
+    let{username, useremail,userpassword}=req.body;
+    console.log(username,useremail,userpassword);
+    let newuser={
+        name:username,
+        email:useremail,
+        password:userpassword
+    }
+    userdata.push(newuser);
+    res.send(userdata);
+})
 app.listen(4545,()=>{
     console.log("server started..");
 })
